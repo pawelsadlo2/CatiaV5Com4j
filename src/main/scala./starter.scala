37eb2c6221dc03fb87992c4624d1c32d77478134
Pawel Sadlo <pawelsadlo2@gmail.com>
2019-04-10 22:12:26 +0200

    minor changes

--- src/main/scala./starter.scala
+++ src/main/scala./starter.scala
@@ -1,42 +1,30 @@
 import CatiaV5TypeLibs.InfItfTypeLib._
 import com4j._
-import com4jExtensions.toScalaConverters
 
 import scala.collection.JavaConverters
-
-
 import scala.util.{Failure, Success, Try}
 
 object starter {
-
   def main(args: Array[String]): Unit = {
 
-    // val libFile:String = System.getProperty("os.arch")//.equals("amd64")// ? "jacob-1.17-x64.dll" : "jacob-1.17-x86.dll"
+
+    val Rot = COM4J.getROT
 
     val catiaApp = getCatiaApp
+
+
     val ROT = JavaConverters.asScalaIterator(COM4J.getROT.iterator()).toList
     val ROT1 = ROT.map(_.queryInterface(classOf[Com4jObject]))
-    val ROT2 = ROT1.map(x => (x, x.is(classOf[Application])))
-
-    val documents1 = catiaApp.documents()
-    //    val documents = new asList(documents1).convertToList
-    //    documents.map(doc => println(doc.queryInterface(classOf[Document]).name()))
-
-    //.util.ResourceBundle.clearCache()
-    //  val selection: Selection = catiaApp.activeDocument().selection().queryInterface(classOf[Selection])
-    // val asMap = new asList(selection)
-    // val mapped = asMap.convertToList
-    // val x =2
-
+    val catiaInstances = ROT1.map(x => (x, x.is(classOf[Application])))
   }
 
   def getCatiaApp(): Application = {
 
-    val excelAppTry = Try(COM4J.getActiveObject(
+    val catiaAppTry = Try(COM4J.getActiveObject(
       classOf[Application],
       "{87fd6f40-e252-11d5-8040-0010b5fa1031}"))
 
-    excelAppTry match {
+    catiaAppTry match {
       case Success(value) => value
       case Failure(_) => {
         val catia = ClassFactory.createApplication()
@@ -47,3 +35,7 @@ object starter {
     }
   }
 }
+
+
+
+
--- src/main/scala/starter.scala
+++ src/main/scala/starter.scala
@@ -1,6 +1,6 @@
 import CatiaV5TypeLibs.InfItfTypeLib._
 import com4j._
-import com4jExtensions.toScalaConverters
+
 import scala.collection.JavaConverters
 import scala.util.{Failure, Success, Try}
 
@@ -18,24 +18,24 @@ object starter {
     val catiaInstances = ROT1.map(x => (x, x.is(classOf[Application])))
   }
 
+  def getCatiaApp(): Application = {
 
-}
+    val catiaAppTry = Try(COM4J.getActiveObject(
+      classOf[Application],
+      "{87fd6f40-e252-11d5-8040-0010b5fa1031}"))
 
+    catiaAppTry match {
+      case Success(value) => value
+      case Failure(_) => {
+        val catia = ClassFactory.createApplication()
+        catia.visible(true)
+        catia
+      }
 
-def getCatiaApp(): Application = {
-
-  val excelAppTry = Try(COM4J.getActiveObject(
-    classOf[Application],
-    "{87fd6f40-e252-11d5-8040-0010b5fa1031}"))
-
-  excelAppTry match {
-    case Success(value) => value
-    case Failure(_) => {
-      val catia = ClassFactory.createApplication()
-      catia.visible(true)
-      catia
     }
-
   }
 }
-}
+
+
+
+
